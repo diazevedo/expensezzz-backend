6e17b2d972e34dc79e0b820756ef405643a9f0c3
Diego Azevedo <dapptn@hotmail.com>
2020-10-10 22:23:33 +1100

    user authentication route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import sessionRoutes from './session';
 import creditorRoutes from './creditor';
 import userRoutes from './user';
 
+import checkTokenMD from '../app/middlewares/jwt';
+
 const routes = new Router();
 
-routes.use(creditorRoutes);
+routes.use(sessionRoutes);
+
+/** At the moment we wont use authentication to user's route as
+ * we just create a session or store a new user
+ */
 routes.use(userRoutes);
+
+/** Auth is required to routes below */
+routes.use(checkTokenMD);
+routes.use(creditorRoutes);
 
 export default routes;
