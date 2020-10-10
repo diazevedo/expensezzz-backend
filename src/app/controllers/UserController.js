f1ad0cecd51a5db2ce4af5ff8c96ef2b362cac21
Diego Azevedo <dapptn@hotmail.com>
2020-10-10 21:26:07 +1100

    user creation validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,14 +1,25 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has failed.' });
+    }
+
     const userExists = await User.findOne({
       where: { email: req.body.email },
     });
 
     if (userExists) {
       return res.status(400).json({
-        error: 'Sorry, this email is registered.',
+        error: 'Sorry, this email is already registered.',
       });
     }
 
